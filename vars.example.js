5ac76bb911f1a6301aa3f39339aad7ca2c1785fa
DJ Rausch <djrausch3@gmail.com>
2015-09-28 20:07:06 -0700

    example vars

---
+++ vars.example.js
@@ -0,0 +1,4 @@
+exports.NEST_USER_NAME = "email@email.com";
+exports.NEST_PASSWORD = "PASSWORD";
+exports.NEST_STRUCTURE_ID = "Structure ID"; //Found from Nest API
+exports.AMAZON_DASH_BUTTON_MAC = "Found from node-dash-button";
